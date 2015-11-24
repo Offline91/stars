65bc76a6fe93c9b654d63677a174b2ef4d110370
offline91 <emilmcle712@ed.amdsb.ca>
2015-11-24 14:41:59 -0500

    new changes

--- stars/Form1.cs
+++ stars/Form1.cs
@@ -7,19 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace stars
 {
     public partial class Form1 : Form
     {
+        private float x;
+        private float y;
+
         public Form1()
         {
             InitializeComponent();
+          
+        }
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            Random randNum = new Random();
+            int y;
+            int x;
+
+            int red, green, blue;
+            red = randNum.Next(1, 256);
+            green = randNum.Next(1, 256);
+            blue = randNum.Next(1, 256);
+
+            x = randNum.Next(1, this.Width);
+            y = randNum.Next(1, this.Width);
+
+            SolidBrush drawbrush = new SolidBrush(Color.FromArgb(red, green, blue, 0));
+       
+
         }
 
-        private void Form1_Click(object sender, EventArgs e)
+        public void starfield(int length)
         {
+            starfield(100);
+            for (length = 100; length<=200; length++ )
+            {
+                Random randNum = new Random();
 
+                x = randNum.Next(1, this.Width);
+                y = randNum.Next(1, this.Height);
+
+                int red, green, blue;
+                red = randNum.Next(1, 256);
+                green = randNum.Next(1, 256);
+                blue = randNum.Next(1, 256);
+                Graphics formgraphics = this.CreateGraphics();
+                SolidBrush drawbrush = new SolidBrush(Color.FromArgb(red, green, blue, 0));
+                formgraphics.FillEllipse(drawbrush, x, y, 25, 25);
+
+                Thread.Sleep(50);
+            }
         }
     }
 }
